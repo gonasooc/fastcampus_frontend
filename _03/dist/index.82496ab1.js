fee260a3f596ecd47853490cdbc6e141a7e26000
pocket.cgs <pocket.cgs@gmail.com>
2022-08-24 08:58:09 +0900

    settings

--- _03/dist/index.82496ab1.js
+++ _03/dist/index.82496ab1.js
@@ -455,18 +455,6 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"hwBw6":[function(require,module,exports) {
-const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-for(let i = 0; i < 10; i++){
-    const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
-    ul.appendChild(li);
-}
-document.getElementById('root').appendChild(ul);
 
 },{}]},["6mFVz","hwBw6"], "hwBw6", "parcelRequire94c2")
 
